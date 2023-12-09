ec7c49050d46951d5758fd0031161a15a6c87b1d
Edmond <edmond.santiago7@gmail.com>
2023-12-09 19:55:45 +1100

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false)
+
+  const itemClass = inCart ? "in-cart" : "";
+
+  function handleAddToCart() {
+    setInCart((inCart) => !inCart)
+  }
+
+  const buttonText = inCart ? "Remove from Cart" : "Add to Cart";
+  const buttonBackgroundColor = inCart ? "hotpink" : "yellow";
+  const buttonColor = inCart ? "white" : "black";
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button style={{ background: buttonBackgroundColor, color: buttonColor }} onClick={handleAddToCart} className="add">{buttonText}</button>
     </li>
   );
 }
